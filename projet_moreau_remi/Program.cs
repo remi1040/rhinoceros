2354fa22e107519bd86ae2cb7d26830889e18a61
remi1040 <48013030+remi1040@users.noreply.github.com>
2021-10-15 00:08:48 +0200

    Add files via upload

---
+++ projet_moreau_remi/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using Aardvark.Base;
+
+namespace projet_rhinoceros
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var dbClient = new MongoClient("mongodb+srv://remi:basemongo@cluster0.bzwiy.mongodb.net/rhinoceros?retryWrites=true&w=majority");
+            var dbList = dbClient.ListDatabases().ToList();
+
+
+            Console.WriteLine("The list of databases on this server is: ");
+            foreach (var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
+
+            var Database = dbClient.GetDatabase("Rhinoceros");
+
+            var reader = new StreamReader(File.OpenRead("C:/Users/remim/Downloads/population_2018.csv"));
+            IMongoCollection<BsonDocument> csvFile = Database.GetCollection<BsonDocument>("Departments");
+
+            reader.ReadLine();
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+
+                BsonDocument row = new BsonDocument
+                        {
+                            {"Code_Post", values[0]},
+                            {"NomDept", values[1]},
+                            {"Pop", values[2]},
+                            {"GPS", values[3]}
+                        };
+
+                csvFile.InsertOne(row);
+            }
+
+            // tentative de récupération des données de la base mongodb
+            /*Console.WriteLine("kjkreoiizjfoijzefjrioretoizutizutozujt---");
+            MongoCollection collection = (MongoCollection)Database.GetCollection<departement>("departements");
+            var blog = new departement()
+            {
+                Id = ObjectId.GenerateNewId().ToString(),
+                Code_Postale = "First Blog"
+            };
+            collection.Insert(blog);
+
+            MongoCursor<departement> cursor = collection.FindAllAs<departement>();
+            cursor.SetLimit(5);
+
+            var list = cursor.ToList();
+            Console.WriteLine("kjkreoiizjfoijzefjrioretoizutizutozujt---");
+            Console.WriteLine(list);*/
+
+
+        }
+
+
+    }
+}
+
+
---
+++ projet_moreau_remi/departement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace projet_rhinoceros
+{
+    //[BsonIgnoreExtraElements]
+    public class departement
+    {
+        [BsonId]
+        //BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        //[BsonElement("Code_Post")]
+        public string Code_Postale { get; set; }
+
+        //[BsonElement("NomDept")]
+        public string DepartmentNom { get; set; }
+
+        //[BsonElement("Pop")]
+        public int Population { get; set; }
+    }
+}
